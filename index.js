1e0b4d86c572aa74310ea8e12a5ec44925ca29e7
DilyanaCh <d.chausheva1718@gmail.com>
2024-11-20 10:08:33 +0100

    Injected the weather details and date data

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
+
+  // humidity - response.data.temperature.humidity
+  // wind = response.data.wind.speed
+
+  //   Humidity: <strong id="humidity">96%</strong>, Wind:
+  // <strong><span id="wind">2.06</span>km/h</strong>
+
+  console.log(response.data);
+
+  console.log(response.data.condition.description);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "ddff3a3f7b48902404oatcfa308e9e5b";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value); // Call searchCity here
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearch);
+searchFormElement.addEventListener("submit", handleSearch); // Because the function handleSearch is triggered when the form is being submitted and that is why we save the form to JS variable and add event lister to it
